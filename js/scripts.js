dbbafd238749863095a5c60412f89c5a817e6026
Alister Fenix & Karvari Ellingson <student@epicodus.com>
2015-02-12 15:55:21 -0800

    count and sort string; pre-spec pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,36 @@
 var wordOrder = function(paragraph) {
   var paragraph = paragraph.toLowerCase().split(" ");
-  var sortedWords = paragraph.sort();
-  return sortedWords;
+  var uniques = [];
+  var i = 0;
+  var counter = 0;
+  var wordCounter = {};
+  var listWords = [];
+
+  for(i=0; i < paragraph.length; i++) {
+    if ((i === paragraph.indexOf(paragraph[i])) || paragraph.indexOf(paragraph[i]) === paragraph.lastIndexOf(paragraph[i])) {
+      uniques.push(paragraph[i]);
+    }
+  }
+
+  uniques.forEach(function(word) {
+    paragraph.forEach(function(old_word) {
+      if (word === old_word) {
+        counter = counter + 1;
+      }
+    });
+
+    wordCounter[counter] = word;
+
+    listWords.push(counter + ", " + wordCounter[counter]);
+
+    counter = 0;
+  });
+
+  listWords.sort();
+
+  return listWords;
 };
 
-
-
-  // var paragraph = paragraph.toLowerCase();
-  // return paragraph;
 // $(document).ready(function() {
 //   $("form#word-order").submit(function(event) {
 //     var number = ($("input#paragraph").val());
--- specs/specs.js
+++ specs/specs.js
@@ -3,16 +3,16 @@ describe("wordOrder", function() {
     expect(wordOrder("are words")).to.eql(["are", "words"]);
   });
 
-  it("will order the string alphabetically", function() {
-    expect(wordOrder("words are really really cool")).to.eql(["are", "cool", "really", "really", "words"]);
+  it("will remove duplicates from the array of words", function() {
+    expect(wordOrder("happy happy joy joy")).to.eql(["happy", "joy"]);
   });
 
   it("will lowercase all letters of a word", function() {
-    expect(wordOrder("Words are REALLY really coOl")).to.eql(["are", "cool", "really", "really", "words"]);
+    expect(wordOrder("Words are REALLY coOl")).to.eql(["words", "are",  "really", "cool"]);
   });
 
-  it("will remove duplicates from the array of words", function() {
-    expect(wordOrder("words are really really cool")).to.eql(["are", "cool", "really", "words"]);
+  it("will order the unique words of the string - by frequency", function() {
+    expect(wordOrder("joy joy happy happy happy fun")).to.eql(["happy", "joy", "fun"])
   });
 
 });
